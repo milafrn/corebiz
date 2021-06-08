db0b3af37cac47ba1a10f3fd5e72e94b857894f1
Mila <kaah.fernandess@hotmail.com>
2021-06-08 20:41:33 -0300

    Fix index app react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react"
-import ReactDom from "react-dom"
-import App from "./App"
-import "./App.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
 
-ReactDom.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
